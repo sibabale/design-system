38756bd89991625cae824a143b4e05d8d6955e06
sibabale <22472288+zbisj@users.noreply.github.com>
2023-02-05 16:20:49 +0200

    fix: remove non-existant export

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import * as styles from "./styles/global.styles";
 import * as global from "./shared/global";
-
 import * as sizes from "./theme/sizes.json";
 import * as colors from "./theme/colors.json";
 // import * as animation from "./shared/animation";
 // import * as icons from "./shared/icons";
 
-export { sizes, theme, colors, styles, global };
+export { sizes, colors, styles, global };
 
 export * from "./components/atoms/button/button";
 export * from "./components/molecules/nav_bar";
